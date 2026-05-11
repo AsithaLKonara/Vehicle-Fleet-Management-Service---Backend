1f930a72215330d7cbc77c60e7b23612f9b8fa83
AsithaLKonara <asithalakmalkonara11992081@gmail.com>
2026-05-11 15:07:02 +0530

    fix: resolve TypeScript type mismatch in serviceRecordController

--- src/controllers/serviceRecordController.ts
+++ src/controllers/serviceRecordController.ts
@@ -3,11 +3,11 @@ import * as serviceRecordService from '../services/serviceRecordService';
 import { asyncWrapper } from '../utils/asyncWrapper';
 
 export const getVehicleServices = asyncWrapper(async (req: Request, res: Response) => {
-  const records = await serviceRecordService.getServiceRecordsByVehicle(req.params.id);
+  const records = await serviceRecordService.getServiceRecordsByVehicle(req.params.id as string);
   res.status(200).json({ success: true, data: records });
 });
 
 export const addVehicleService = asyncWrapper(async (req: Request, res: Response) => {
-  const record = await serviceRecordService.createServiceRecord(req.params.id, req.body);
+  const record = await serviceRecordService.createServiceRecord(req.params.id as string, req.body);
   res.status(201).json({ success: true, data: record });
 });
